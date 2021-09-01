0a0ce383f24ccadcf6fa2ebb750b1cfa1c41a5a3
wardogordo <wardh@me.com>
2021-08-31 22:56:21 -0500

    Commit me.

--- learncppdotcom/ch-07-control-flow-error-handling/testing/main.cpp
+++ learncppdotcom/ch-07-control-flow-error-handling/testing/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
- char c{};
+
+int ExponentTotal(int base, int power)
+{
+    int total{1};
+
+    for (int i = 0; i < power; ++i)
+    {
+        total *= base;
+        std::cout << "Incremental total: " << total << "\n";
+    }
+    std::cout << "Total: " << total;
+    return 0;
+}
 
 int main()
 {
-
-    do
-    {
-        std::cout << "Please enter a vowel: ";
-        std::cin >> c;
-    }
-    while (c != 'a' && c != 'A');
-
-    std::cout << "That's correct.";
-
-    return 0;
+    ExponentTotal(5, 4);
 }
