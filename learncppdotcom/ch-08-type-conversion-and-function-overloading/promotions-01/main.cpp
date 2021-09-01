1c1ed3484d3a54056761d8bac4682fd429c574a5
wardogordo <wardh@me.com>
2021-09-01 14:39:34 -0500

    First commit of chapter 8.

--- learncppdotcom/ch-08-type-conversion-and-function-overloading/promotions-01/main.cpp
+++ learncppdotcom/ch-08-type-conversion-and-function-overloading/promotions-01/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+int MinFundamentalSizesCpp()
+{
+    std::cout << "Minimum C++ size guarantees of fundamental types:\n";
+    std::cout << "Character:\t " << sizeof(char) << " byte.\n";
+    std::cout << "Short:\t\t " << sizeof(short) << " bytes.\n";
+    std::cout << "Float:\t\t " << sizeof(float) << " bytes.\n";
+    std::cout << "Double:\t\t " << sizeof(double) << " bytes.\n";
+    std::cout << "Long Double:\t " << sizeof(long double) << " bytes.\n";
+
+    return 0;
+}
+
 int main()
 {
-    std::cout << "Double: " << sizeof(double) << " bytes.";
-    std::cout << "Float: " << sizeof(float) << " bytes.";
+    MinFundamentalSizesCpp();
+
     return 0;
 }
