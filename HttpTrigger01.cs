1d760b7be4b164e176aa93a1cae417b53441dce2
Ward Holmquist <wardh@me.com>
2021-09-15 17:52:04 -0500

    Deployed to Azure account

--- HttpTrigger01.cs
+++ HttpTrigger01.cs
@@ -27,7 +27,7 @@ namespace Tradegame.Function
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, my delightful friend, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
