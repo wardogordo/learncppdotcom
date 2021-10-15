f0ad27472e43169cc50079a5e15238768bfce0be
wardogordo <wardh@me.com>
2021-10-15 10:10:21 -0500

    Working with friend keyword

--- learncppdotcom/ch-12-basic-oop/friend-functions/main.cpp
+++ learncppdotcom/ch-12-basic-oop/friend-functions/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+class Accumulator
+{
+private:
+    int m_value;
+public:
+    Accumulator() { m_value = 0; }
+    void add(int value) { m_value += value; }
+    int GetValue()
+    {
+        return m_value;
+    }
+
+    friend void reset(Accumulator& accumulator);
+};
+
+void reset(Accumulator& accumulator)
+{
+    accumulator.m_value = 0;
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Accumulator acc;
+    std::cout << acc.GetValue() << '\n';
+    acc.add(5);
+    std::cout << acc.GetValue() << '\n';
+    reset(acc);
+    std::cout << acc.GetValue() << '\n';
+
     return 0;
 }
