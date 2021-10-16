19ccc82f2242839b424bf5235039a2aa8ad0b946
wardogordo <wardh@me.com>
2021-10-16 12:53:40 -0500

    More friends keyword work

--- learncppdotcom/ch-12-basic-oop/friend-functions/main.cpp
+++ learncppdotcom/ch-12-basic-oop/friend-functions/main.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 
-class Accumulator
+class Value
 {
 private:
     int m_value;
 public:
-    Accumulator() { m_value = 0; }
-    void add(int value) { m_value += value; }
-    int GetValue()
-    {
-        return m_value;
-    }
+    Value(int value) { m_value = value; }
 
-    friend void reset(Accumulator& accumulator);
+    friend int readMyValue(Value& value);
+
+    friend void resetMyValue(Value& value);
+
+    friend void changeMyValue(Value& value, int new_m_value);
 };
 
-void reset(Accumulator& accumulator)
+int readMyValue(Value& value) { return value.m_value; }
+void resetMyValue(Value& value) { value.m_value = 0; }
+void changeMyValue(Value& value, int new_m_value)
 {
-    accumulator.m_value = 0;
+    value.m_value = new_m_value;
 }
 
 int main()
 {
-    Accumulator acc;
-    std::cout << acc.GetValue() << '\n';
-    acc.add(5);
-    std::cout << acc.GetValue() << '\n';
-    reset(acc);
-    std::cout << acc.GetValue() << '\n';
+    Value myValue(5);
+    std::cout << "myValue: " << readMyValue(myValue) << '\n';
+    resetMyValue(myValue);
+    std::cout << "myValue: " << readMyValue(myValue) << '\n';
+    changeMyValue(myValue, 54);
+    std::cout << "myValue is now now: " << readMyValue(myValue) << '\n';
+
 
     return 0;
 }
