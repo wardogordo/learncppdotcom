a75f96eb6b35c6784bf08b740608f2c66be45c21
wardogordo <wardh@me.com>
2021-10-16 17:24:37 -0500

    Second commit

--- learncppdotcom/ch-10-intro-to-pointers/references02/main.cpp
+++ learncppdotcom/ch-10-intro-to-pointers/references02/main.cpp
@@ -2,14 +2,27 @@
 
 class Study
 {
+private:
+    int m_myInteger;
+public:
+    Study(int myInteger) { m_myInteger = myInteger;}
 
+    void setMyInteger(int myInt) { m_myInteger = myInt;}
+    int getMyInteger() { return m_myInteger;}
 };
 
 
 
 int main()
 {
+    Study hoozle = Study(76);
+    Study hizzle = Study(44);
 
+    std::cout << "Hoozle: " << hoozle.getMyInteger() << '\n';
+    std::cout << "Hizzle: " << hizzle.getMyInteger() << '\n';
+
+    hoozle.setMyInteger(67);
+    std::cout << "Hoozle: " << hoozle.getMyInteger() << '\n';
 
     return 0;
 }
