31a23589a4edd5c3230f93707846e020f518a6d5
wardogordo <wardh@me.com>
2021-10-17 00:50:14 -0500

    EOD commit

--- learncppdotcom/ch-10-intro-to-pointers/references02/main.cpp
+++ learncppdotcom/ch-10-intro-to-pointers/references02/main.cpp
@@ -24,5 +24,15 @@ int main()
     hoozle.setMyInteger(67);
     std::cout << "Hoozle: " << hoozle.getMyInteger() << '\n';
 
+    int myPublicInt { 11 };
+    std::cout << "myPublicInt: " << myPublicInt << '\n';
+
+    int& myRefVar = myPublicInt;
+    int myNonRefVar = myPublicInt;
+
+    std::cout << "myPublicInt: " << &myPublicInt << '\n';
+    std::cout << "myRefVar: " << &myRefVar << '\n';
+    std::cout << "myNonRefVar: " << &myNonRefVar << '\n';
+
     return 0;
 }
