68f0e20b025dd1cf3bb1d9d51bd31c0a8236167a
wardogordo <wardh@me.com>
2021-10-20 10:44:33 -0500

    First session with operator+ overloading

--- learncppdotcom/ch-13-operator-overloading/13-2-oo-with-friend-functions/main.cpp
+++ learncppdotcom/ch-13-operator-overloading/13-2-oo-with-friend-functions/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
+class Cents
+{
+private:
+    int m_cents{};
+
+public:
+    Cents(int cents) { m_cents = cents;}
+
+    friend Cents operator+(const Cents &c1, const Cents &c2);
+
+    int getCents() const { return m_cents; }
+};
+
+Cents operator+(const Cents &c1, const Cents &c2)
+{
+    return Cents(c1.m_cents + c2.m_cents);
+}
+
+
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Cents cents1{ 6 };
+    Cents cents2{ 8 };
+    Cents centsSum{ cents1 + cents2 };
+    std::cout << "I have " << centsSum.getCents() << " cents.\n";
+
     return 0;
 }
