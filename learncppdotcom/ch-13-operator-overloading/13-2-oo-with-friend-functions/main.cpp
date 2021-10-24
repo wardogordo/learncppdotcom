b330d3b16a85e626d6a69d449d59d0e7ecf337b0
wardogordo <wardh@me.com>
2021-10-24 09:54:48 -0500

    Sunday morning work.

--- learncppdotcom/ch-13-operator-overloading/13-2-oo-with-friend-functions/main.cpp
+++ learncppdotcom/ch-13-operator-overloading/13-2-oo-with-friend-functions/main.cpp
@@ -3,29 +3,35 @@
 class Cents
 {
 private:
-    int m_cents{};
+    int m_cents {};
 
 public:
-    Cents(int cents) { m_cents = cents;}
-
-    friend Cents operator+(const Cents &c1, const Cents &c2);
-
+    Cents(int cents) { m_cents = cents; }
     int getCents() const { return m_cents; }
+
+    friend Cents operator+(Cents const &c1, Cents const &c2);
+    friend Cents operator-(Cents const &c1, Cents const &c2);
+
 };
 
-Cents operator+(const Cents &c1, const Cents &c2)
+Cents operator+(Cents const &c1, Cents const &c2)
 {
     return Cents(c1.m_cents + c2.m_cents);
 }
 
-
+Cents operator-(Cents const &c1, Cents const &c2)
+{
+    return Cents(c1.m_cents - c2.m_cents);
+}
 
 int main()
 {
-    Cents cents1{ 6 };
-    Cents cents2{ 8 };
-    Cents centsSum{ cents1 + cents2 };
-    std::cout << "I have " << centsSum.getCents() << " cents.\n";
+    Cents centsOne(11);
+    Cents centsTwo(7);
+    Cents centsSum{ centsOne + centsTwo };
+    std::cout << "My Cents objects addition is: " << centsSum.getCents() << '\n';
+    Cents centsDifference{ centsOne - centsTwo };
+    std::cout << "My Cents objects subtraction is: " << centsDifference.getCents() << '\n';
 
     return 0;
 }
