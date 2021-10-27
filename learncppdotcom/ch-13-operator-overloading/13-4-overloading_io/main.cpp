145d09493ee3a4bb6b1934d831bfa987dbea68a9
wardogordo <wardh@me.com>
2021-10-27 07:51:16 -0500

    First commit.

--- learncppdotcom/ch-13-operator-overloading/13-4-overloading_io/main.cpp
+++ learncppdotcom/ch-13-operator-overloading/13-4-overloading_io/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
+class Point
+{
+private:
+    double m_x{};
+    double m_y{};
+    double m_z{};
+
+public:
+    Point(double x=0.0, double y=0.0, double z=0.0)
+        : m_x{x}, m_y{y}, m_z{z}
+    {
+    }
+
+    double getX() const { return m_x; }
+    double getY() const { return m_y; }
+    double getZ() const { return m_z; }
+};
+
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Point point{5.0, 6.0, 7.0};
+
+    std::cout << "Point(" << point.getX() << ")" << '\n';
+
     return 0;
 }
