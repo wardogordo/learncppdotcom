7eab9d6e7eeca286c0edbdcdc832e5397e254239
wardogordo <wardh@me.com>
2021-10-31 10:16:52 -0500

    Commit after partial completion of lesson

--- learncppdotcom/ch-16-intro-to-obj-relations/16-3-aggregation/main.cpp
+++ learncppdotcom/ch-16-intro-to-obj-relations/16-3-aggregation/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <string>
+
+class Teacher
+{
+private:
+    std::string m_name{};
+
+public:
+    Teacher(const std::string& name)
+        : m_name{ name }
+    {
+    }
+
+    const std::string& getName() const { return m_name;}
+};
+
+class Department
+{
+private:
+    const Teacher& m_teacher;
+
+public:
+    Department(const Teacher& teacher)
+        : m_teacher{ teacher }
+    {
+    }
+
+   const Teacher getTeacher() const { return m_teacher; };
+};
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Create a teacher outside the scope of the Department
+    Teacher bob{ "Bob" };
+
+    {
+//        Create a department and use the constructor parameter
+//          to pass the teacher to it.
+        Department department(bob);
+        std::cout << "The department Department has a teacher named "
+        << department.getTeacher().getName() << ".\n";
+    } // department goes out of scope here and is destroyed
+
+    // bob stilll exists here, but the department doesn't
+
+    std::cout << bob.getName() << " still exists!\n";
+
     return 0;
 }
