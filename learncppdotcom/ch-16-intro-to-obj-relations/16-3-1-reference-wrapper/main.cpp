364652db022221a6757323e092494a03d44a7021
wardogordo <wardh@me.com>
2021-11-01 12:41:50 -0500

    Completed lesson.

--- learncppdotcom/ch-16-intro-to-obj-relations/16-3-1-reference-wrapper/main.cpp
+++ learncppdotcom/ch-16-intro-to-obj-relations/16-3-1-reference-wrapper/main.cpp
@@ -1,7 +1,28 @@
+#include <functional> // std::reference_wrapper
 #include <iostream>
+#include <vector>
+#include <string>
 
+// Here’s an example using std::reference_wrapper in a std::vector:
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::string tom{ "Tom" };
+    std::string berta{ "Berta" };
+
+    std::vector<std::reference_wrapper<std::string>> names{ tom, berta }; // Stored by reference, not value
+
+    std::string  jim{ "Jim" };
+
+    names.push_back(jim);
+
+    for (auto name : names)
+    {
+        // Use the get() member function to get the referenced string.
+        name.get() += " Beam";
+        std::cout << name.get() << '\n';
+    }
+
+
+
     return 0;
 }
