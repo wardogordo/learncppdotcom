a999d66c6cd8fef9ffb0cf2dd58cfa0de363b712
wardogordo <wardh@me.com>
2021-11-05 09:09:39 -0500

    First example recreated.

--- learncppdotcom/ch-17-inheritance/17_4-constructors-and-initialization-of-derived-classes/main.cpp
+++ learncppdotcom/ch-17-inheritance/17_4-constructors-and-initialization-of-derived-classes/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <string>
+
+class Base
+{
+private:
+    int m_Id{};
+
+public:
+    Base(int id=0)
+        : m_Id{id}
+    {
+    }
+
+    int getId() const { return m_Id; }
+};
+
+class Derived : public Base
+{
+private:
+    double m_cost{};
+
+public:
+    Derived(int id, double cost)
+        : m_cost{ cost }
+        , Base{ id }
+    {
+    }
+
+    double getCost() const { return  m_cost; };
+};
+
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Derived derived(24, 39.99);
+    std::cout << "derived: id " << derived.getId()  << "; cost $" << derived.getCost() << '\n';
     return 0;
 }
