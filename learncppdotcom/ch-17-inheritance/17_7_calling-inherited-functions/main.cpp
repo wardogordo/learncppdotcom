29a51fecb0b07d55202c9e0e1af2aca6c1759c7b
wardogordo <wardh@me.com>
2021-11-08 06:52:19 -0600

    Finished.

--- learncppdotcom/ch-17-inheritance/17_7_calling-inherited-functions/main.cpp
+++ learncppdotcom/ch-17-inheritance/17_7_calling-inherited-functions/main.cpp
@@ -2,35 +2,36 @@
 
 class Base
 {
-protected:
+private:
     int m_value{};
 
 public:
     Base(int value)
-        : m_value{ value }
+        : m_value(value)
     {
     }
 
-    void identify() const { std::cout << "I am a Base.\n"; }
+    void identify() const { std::cout << "I am Base, y'all.\n";}
 };
 
 class Derived : public Base
 {
 public:
     Derived(int value)
-        : Base{ value }
+        : Base(value)
     {
     }
 
-    void identify() const { std::cout << "I am a Derived.\n";}
+    void identify() const {
+        Base::identify();
+        std::cout << "I'm Derived and cool.\n";}
 };
 
 int main()
 {
-    Base base(6);
-    base.identify();
-    Derived derived{ 11 };
-    derived.identify();
-
+//    Base base1(6);
+//    base1.identify();
+    Derived derived1(42);
+    derived1.identify();
     return 0;
 }
