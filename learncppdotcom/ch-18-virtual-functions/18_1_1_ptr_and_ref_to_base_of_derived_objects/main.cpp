49e69c8fc5a4edda5a448dc7addfaa3db7e67eb6
wardogordo <wardh@me.com>
2021-11-10 08:16:40 -0600

    Morning work 2021-11-10_08:16.

--- learncppdotcom/ch-18-virtual-functions/18_1_1_ptr_and_ref_to_base_of_derived_objects/main.cpp
+++ learncppdotcom/ch-18-virtual-functions/18_1_1_ptr_and_ref_to_base_of_derived_objects/main.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include <string_view>
+#include <string>
+
+class Animal
+{
+protected:
+    std::string m_name{};
+
+    Animal(std::string_view name)
+        : m_name{ name }
+    {
+    }
+
+    // Not sure what this does. He says: "To prevent slicing (covered later)"
+    Animal(const Animal&) = default;
+    Animal& operator=(const Animal&) = default;
+
+public:
+    std::string_view getName() const { return m_name; }
+    std::string_view speak() const { return "???";}
+};
+
+class Cat : public Animal
+{
+public:
+    Cat(std::string_view name)
+        : Animal {name}
+    {
+    }
+
+    std::string_view speak() const { return "Meow. Just."; }
+};
+
+class Dog : public Animal
+{
+public:
+    Dog(std::string_view name)
+        : Animal{ name }
+    {
+    }
+
+    std::string_view speak() const { return "Wooflike."; }
+};
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Cat cat1{"Hortensius"};
+    std::cout << cat1.getName() << " is a cat and says \"" << cat1.speak() << "\"\n";
+
+    Dog dog1{ "Ralph" };
+    std::cout << dog1.getName() << " is a dog and says \"" << dog1.speak() << "\"\n";\
+
+    const Animal* pAnimal{ &cat1 };
+    std::cout << "pAnimal is named " << pAnimal->getName() << " and says \"" << pAnimal->speak() << "\"\n";
+
+    pAnimal = &dog1;
+    std::cout << "pAnimal is named " << pAnimal->getName() << " and says \"" << pAnimal->speak() << "\"\n";
+
     return 0;
 }
