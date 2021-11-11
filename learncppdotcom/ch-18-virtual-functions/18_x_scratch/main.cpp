b499f513c51320ae5110f23382af90b0c0a9fd9b
wardogordo <wardh@me.com>
2021-11-11 12:38:44 -0600

    EOD.

--- learncppdotcom/ch-18-virtual-functions/18_x_scratch/main.cpp
+++ learncppdotcom/ch-18-virtual-functions/18_x_scratch/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+class Working
+{
+private:
+    bool m_IAmWorking{};
+public:
+    Working(bool working)
+        : m_IAmWorking{ working }
+    {
+    }
+};
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Working working(true);
+    auto amISize = sizeof(working);
+    std::cout << amISize << '\n';
+
     return 0;
 }
