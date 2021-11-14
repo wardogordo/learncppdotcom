8a5fa54e6a13d6ac1646e9ea95496223a5a6a564
wardogordo <wardh@me.com>
2021-11-14 09:56:51 -0600

    Final commit of morning.

--- learncppdotcom/ch-18-virtual-functions/18_9_object-slicing/main.cpp
+++ learncppdotcom/ch-18-virtual-functions/18_9_object-slicing/main.cpp
@@ -23,5 +23,19 @@ public:
     {
     }
 
-    virtual const char* getName() const { return "Derived"; }
+    const char* getName() const override { return "Derived"; }
 };
+
+int main()
+{
+    Derived derived1 { 22 };
+    Base* base1{ &derived1 };
+    std::cout << "base1 is a " << base1->getName() << " with value " << base1->getValue() << '\n';
+
+    Derived derived2 { 5 };
+    Base base2 { derived2 };
+    std::cout << "base2 is a " << base2.getName() << " with value " << base2.getValue() << '\n';
+
+
+    return 0;
+}
