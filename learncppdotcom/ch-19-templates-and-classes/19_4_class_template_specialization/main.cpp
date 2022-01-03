1e41e1d9350ebe35e122bde953ae19866ab57c19
wardogordo <wardh@me.com>
2022-01-03 09:49:57 -0600

    Finished first section with bitwise operator &

--- learncppdotcom/ch-19-templates-and-classes/19_4_class_template_specialization/main.cpp
+++ learncppdotcom/ch-19-templates-and-classes/19_4_class_template_specialization/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 
+template <typename T>
+class Storage8
+{
+private:
+    T m_array[8];
+
+public:
+    void set(int index, const T &value)
+    {
+        m_array[index] = value;
+    }
+
+    const T& get(int index) const
+    {
+        return m_array[index];
+    }
+
+};
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Define a Storage8 for integers
+    Storage8<int> intStorage;
+
+    for (int count{ 0 }; count < 8; ++count)
+        intStorage.set(count, count);
+
+    for (int count{ 0 }; count < 8; ++count)
+        std::cout << intStorage.get(count) << '\n';
+
+    // Define a Storage8 for bool
+    Storage8<bool> boolStorage;
+
+    for (int count{ 0 }; count < 8; ++count)
+        boolStorage.set(count, count & 3);
+
+    std::cout << std::boolalpha;
+
+    for (int count = 0; count < 8; ++count)
+    {
+        std::cout << boolStorage.get(count) <<'\n';
+    }
+
     return 0;
 }
