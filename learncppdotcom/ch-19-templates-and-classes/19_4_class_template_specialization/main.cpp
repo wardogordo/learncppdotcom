211d54eca41966e543f44c96e3c86b6caf4b99bd
wardogordo <wardh@me.com>
2022-01-05 08:50:36 -0600

    Finished this section. Learned a couple bitwise operations, a first for me.

--- learncppdotcom/ch-19-templates-and-classes/19_4_class_template_specialization/main.cpp
+++ learncppdotcom/ch-19-templates-and-classes/19_4_class_template_specialization/main.cpp
@@ -16,12 +16,55 @@ public:
     {
         return m_array[index];
     }
+};
+
+template <> // Template class with no templated parameters
+class Storage8<bool> // This template class uses the same name, but just handles bools.
+{
+private:
+    unsigned char m_data{};
+
+public:
+    void set(int index, bool value)
+    {
+        // Figure out which bit we're getting
+        auto mask{ 1 << index };
+
+        if (value) // If we're setting a bit
+            m_data |= mask; //Use bitwise-or to turn that bit on
+        else
+            m_data &= ~mask; // bitwise-and the inverse mask to turn that bit off
+    }
+
+    bool get(int index)
+    {
+        // Figure out which bit we're getting
+        auto mask{ 1 << index };
+        // bitwise-and to get the value of the bit we're interested in
+        // Then implicit cast to boolean
+        return (m_data & mask);
+    }
 
 };
 
+void checkAlice()
+{
+
+    bool alice{ false };
+    if (alice)
+        std::cout << "Alice" << '\n';
+    else
+        std::cout << "no Alice" << '\n';
+}
+
+
+void checkAlice();
+
 int main()
 {
-    // Define a Storage8 for integers
+  checkAlice();
+
+    // Define a Storage8 for integers (instantiates Storage8<T>, where T = int)
     Storage8<int> intStorage;
 
     for (int count{ 0 }; count < 8; ++count)
@@ -30,19 +73,20 @@ int main()
     for (int count{ 0 }; count < 8; ++count)
         std::cout << intStorage.get(count) << '\n';
 
-    // Define a Storage8 for bool
+    // Define a Storage8 for bool (instantiates Storage8<bool> specialization)
     Storage8<bool> boolStorage;
 
-    for (int count{ 0 }; count < 8; ++count)
+    for (int count = 0; count < 8; ++count)
+    {
         boolStorage.set(count, count & 3);
+    }
 
     std::cout << std::boolalpha;
 
     for (int count = 0; count < 8; ++count)
     {
-        std::cout << boolStorage.get(count) <<'\n';
+        std::cout << boolStorage.get(count) << '\n';
     }
 
-
-    return 0;
+return 0;
 }
